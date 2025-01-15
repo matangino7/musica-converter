275678e806f37e59569e6d5a831dce6acb8e4229
eliya188 <eliyatzanani188@gmail.com>
2025-01-15 15:55:40 +0200

    added spotify connect button

--- frontend-/musica-converter/src/app/app.module.ts
+++ frontend-/musica-converter/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpotifyButtonComponent } from './spotify-button/spotify-button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpotifyButtonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
